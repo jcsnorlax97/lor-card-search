f4ef609210ae7576ccf46d1fffef28b1fc9a696e
Justin <justin.choi.developer@gmail.com>
2020-05-06 21:39:28 -0500

    Create a simple greeting app connecting ReactJS (frontend) and ExpressJS (backend) together, confirming the app is working properly

--- lor-card-search/src/App.js
+++ lor-card-search/src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Form from "./Form";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    formName: "",
+    formGreeting: "",
+  };
+
+  handleFormChange = (event) => {
+    const target = event.target;
+    const value = target.value; // value to be stored
+    const name = target.name; // key used to accessing state (based on the 'name' attribute in <form><input name="XXX" .../></form>)
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  handleFormSubmit = (event) => {
+    event.preventDefault(); // prevent page auto reload
+    fetch(`/api/greeting?name=${encodeURIComponent(this.state.formName)}`)
+      .then((response) => response.json())
+      .then((jsonObj) => {
+        console.log(jsonObj);
+        this.setState({
+          formGreeting: jsonObj["greeting"],
+        });
+      });
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <Form
+          formName={this.state.formName}
+          onFormChange={this.handleFormChange}
+          onFormSubmit={this.handleFormSubmit}
+        />
+        {this.state.formGreeting}
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
